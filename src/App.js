be8cddd4ee247d079ce5edbd90374da46c9d6611
SaadFarhanIdress <sunehrafarhan123@gmail.com>
2021-02-24 03:34:57 +0500

    Add many things in App.js. See description for details.
    I first imported all of the created components and used all of them.
    I created a class component and passed two things to state.
    Then I created an async componentDidMount function that have the fetchedData const which contains the evoked function and it will evoke many times because of componentDidMount then I set the fetchedData const as the value of data state.
    Then I created handleCountryChange async function that have one parameter which is country and the country will be passed as a parameter to fetchData function which is imported from ./api and the it have the fetchedData const which contains the evoked function with an await keyword.
    Then I passed country from the state to Chart component as props and data also.
    Added image on the top.
    Passed data to Card component.
    Passed handleCountryChange to CountryPicker.
    Added credits at the bottom.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import bannerImage from './image/banner.png';
+import { Cards, Chart, CountryPicker } from "./components/store";
+import styles from './App.module.css';
+import { fetchData } from "./api";
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData })
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country })
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={bannerImage} alt='COVID-19' />
+        <Cards data={data} /><br />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+        <p className="credit" style={{textAlign: 'center'}}>Made with <img className="image" width="20" height="20" alt="Heart" src="https://cdn.iconscout.com/icon/free/png-256/heart-1767836-1502416.png" /> by <a href="www.github.com/SaadFarhanIdress">Saad Farhan</a>
+      <br />
+      Don't forget to star my repository :) <br />Link: <a href="https://github.com/SaadFarhanIdress/project-2-covid-tracker">https://github.com/SaadFarhanIdress/project-2-covid-tracker</a></p>
+        </div>
+    );
+  }
 }
 
 export default App;
